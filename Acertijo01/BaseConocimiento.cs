518a7d2bba7ed67d0e325cdd910544c7a335049b
SiddharthaGF <pablo.guerra@espoch.edu.ec>
2022-06-19 10:54:51 -0500

    Agregar archivos de proyecto.

---
+++ Acertijo01/BaseConocimiento.cs
@@ -0,0 +1,68 @@
+﻿namespace Acertijo01
+{
+    internal class BaseConocimiento
+    {
+
+        public class Estado
+        {
+            public IList<string> estados = new string[4];
+            public Estado() { }
+            public Estado(string est1, string est2, string est3, string est4) => estados = new string[] { est1, est2, est3, est4 };
+        }
+
+        private class Direccion
+        {
+            private IList<string> direcciones = new string[2];
+            public Direccion() { }
+            public Direccion(string dir1, string dir2) => direcciones = new string[] { dir1, dir2 };
+        }
+
+        public class ResolverAcertijo
+        {
+            private List<Estado> estados = new();
+            private List<Estado> prohibidos = new();
+            private List<Estado> movimientos = new();
+            private List<Direccion> direcciones = new();
+
+            public ResolverAcertijo()
+            {
+
+            }
+
+            public void AnadirEstado(string est1, string est2, string est3, string est4) => estados.Add(new Estado(est1, est2, est3, est4));
+            public void AnadirEstadoProhibido(string est1, string est2, string est3, string est4) => prohibidos.Add(new Estado(est1, est2, est3, est4));
+            public void AnadirDireccion(string dir1, string dir2) => direcciones.Add(new Direccion(dir1, dir2));
+            public bool DetectarEstadosProhibido(Estado estados) => !prohibidos.Contains(estados);
+            public static void Escribir(string linea) => Console.WriteLine(linea);
+
+            public static bool ComprobarMeta(IList<string> actual, IList<string> final)
+            {
+                var actualyfinal = actual.Zip(final, (n, w) => new { Actual = n, Final = w });
+                foreach (var estado in actualyfinal)
+                    if (estado.Actual != estado.Final) return false;
+                return true;
+            }
+
+            public void MostrarMovimientos()
+            {
+                foreach (var movimiento in movimientos)
+                    Escribir(ConcatenarEstados(movimiento.estados));
+            }
+
+            public string ConcatenarEstados(IList<string> estado)
+            {
+                string linea = string.Empty;
+                foreach (var est in estado)
+                    linea += est + " ";
+                return linea;
+            }
+
+            public void Resolver(Estado Inicial, Estado Final)
+            {
+
+            }
+
+        }
+
+    }
+}
---
+++ Acertijo01/Program.cs
@@ -0,0 +1,30 @@
+﻿using static Acertijo01.BaseConocimiento;
+
+ResolverAcertijo acertijo = new();
+
+var izq = "izquierda";
+var der = "derecha";
+
+acertijo.AnadirDireccion(izq, der);
+acertijo.AnadirDireccion(der, izq);
+
+acertijo.AnadirEstado(der, der, der, der);
+acertijo.AnadirEstado(der, der, izq, izq);
+acertijo.AnadirEstado(izq, der, izq, izq);
+acertijo.AnadirEstado(der, der, izq, der);
+acertijo.AnadirEstado(izq, izq, izq, der);
+acertijo.AnadirEstado(der, izq, der, der);
+acertijo.AnadirEstado(izq, der, izq, der);
+acertijo.AnadirEstado(izq, izq, izq, izq);
+
+acertijo.AnadirEstadoProhibido(izq, der, der, izq);
+acertijo.AnadirEstadoProhibido(der, izq, izq, der);
+acertijo.AnadirEstadoProhibido(izq, izq, der, der);
+acertijo.AnadirEstadoProhibido(der, der, izq, izq);
+acertijo.AnadirEstadoProhibido(der, izq, izq, izq);
+acertijo.AnadirEstadoProhibido(izq, der, der, der);
+
+Estado inicial = new(der, der, der, der);
+Estado final = new(izq, izq, izq, izq);
+
+acertijo.Resolver(inicial, final);
